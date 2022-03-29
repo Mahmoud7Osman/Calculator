3f57a6a57d58a3c349af12d08d579c2a954571ac
Mahmoud Osman <60033959+Mahmoud7Osman@users.noreply.github.com>
2022-03-29 18:11:45 +0300

    Add files via upload

--- js/calculator.js
+++ js/calculator.js
@@ -1,14 +1,54 @@
-const operations       = document.querySelector(".op");
-const numbers          = document.querySelector(".num");
-const screen           = document.getElementById("output");
-const reset            = document.getElementById("ac");
+const operations       = document.querySelectorAll(".op");
+const numbers          = document.querySelectorAll(".num");
+const prev             = document.getElementById("prev");
+const curr             = document.getElementById("curr");
+const rst              = document.getElementById("acl");
 const equal            = document.getElementById("eq");
 
-let number1, number2;
+let cop;
 
-equal.onclick = Compute(prev, next);
-reset.onclick = function(){ screen.innerHTML = '' ; };
+for (let ops in operations){
+    operations[ops].onclick = () => {
+        cop = operations[ops].textContent;
+        prev.innerHTML = curr.innerHTML + " " + operations[ops].textContent;
+        curr.innerHTML = '';
+    }
+}
+
+for (let nums in numbers){
+    numbers[nums].onclick = () => {
+        curr.innerHTML += numbers[nums].textContent;
+    };
+}
+equal.onclick = () => {
+    Compute(prev.textContent, curr.textContent);
+}
+rst.onclick = () =>{
+    curr.innerHTML = '';
+    prev.innerHTML = '';
+};
 
 function Compute(num1, num2){
-    screen.innerHTML = num1 + num2;
+    if (curr.innerHTML == "" || prev.innerHTML == "") return;
+    curr.innerHTML = '';
+    prev.innerHTML = '';
+
+    num1 = parseInt(num1);
+    num2 = parseInt(num2);
+    switch (cop) {
+        case "+":
+            curr.innerHTML = num1 + num2;
+            break;
+        case "-":
+            curr.innerHTML = num1 - num2;
+            break;
+        case "x":
+            curr.innerHTML = num1 * num2;
+            break;
+        case "/":
+            curr.innerHTML = num1 / num2;
+            break;
+        default:
+            break;
+    }
 }
